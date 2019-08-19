e229f60d6bca13430eac2cc050f1c4c71e573342
uabeautifull <torres-dan@yandex.ru>
2019-08-19 13:11:10 +0300

    update sctipts

--- src/db/scripts.go
+++ src/db/scripts.go
@@ -1,14 +1,20 @@
 package db
 
 var CREATE_USERS_IF_NOT_EXISTS = `
-	CREATE TABLE IF NOT EXISTS users (
-	id BIGINT NOT NULL,
-	lang string NOT NULL,
-									)`
+create table if not exists users (
+	id INT NOT NULL,
+   chat_id BIGINT NOT NULL,
+   lang VARCHAR(8),
+   PRIMARY KEY (id)
+);`
 
 
 var CREATE_SALES_IF_NOT_EXISTS = `
-	CREATE TABLE IF NOT EXISTS users (
+create table if not exists sales (
 	id BIGINT NOT NULL,
-	lang string NOT NULL,
-	)`
+	user_id BIGINT NOT NULL,
+	inf VARCHAR(255),
+	sell_at timestamp,
+	PRIMARY KEY (id),
+	FOREIGN KEY (user_id) REFERENCES usets (id)
+);`
